dfe787abf8f892c207d05abfa89b4e4530ff1170
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-11-02 11:36:15 +0100

    adding mail

---
+++ resources/views/mail/published.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body {
+            background: lightblue;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    New project
+</body>
+</html>
